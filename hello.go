ef5a5a69256ba8ee75385b509412f875d937a662
Carlos Guzmán <cguzman@xmltravelgate.com>
2019-03-12 11:23:50 +0100

    New version

--- hello.go
+++ hello.go
@@ -4,5 +4,9 @@ import "fmt"
 
 // SayHello say hello to the world
 func SayHello() {
-	fmt.Println("Hellomod world! Version v0.5.0")
+	fmt.Println("Hellomod world! Version v0.8.0")
+}
+
+func NewSayHello() {
+	fmt.Println("New Hellomod world!!! Version v0.8.0")
 }
