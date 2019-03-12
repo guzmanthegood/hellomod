5cd58ed5129fb82375abbce6720ef83a8eed68f8
Carlos Guzmán <cguzman@xmltravelgate.com>
2019-03-12 11:25:50 +0100

    Bug fix

--- hello.go
+++ hello.go
@@ -4,9 +4,10 @@ import "fmt"
 
 // SayHello say hello to the world
 func SayHello() {
-	fmt.Println("Hellomod world! Version v0.8.0")
+	fmt.Println("Hellomod world! Version v0.8.1 **bugfix**")
 }
 
+// NewSayHello function
 func NewSayHello() {
-	fmt.Println("New Hellomod world!!! Version v0.8.0")
+	fmt.Println("New Hellomod world!!! Version v0.8.1 **bugfix**")
 }
